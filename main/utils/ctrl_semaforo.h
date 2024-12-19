383c9147307aff4fa50d36bfc1dd76904f04b4af
Matheus Vinicius da Costa <ma.usv@hotmail.com>
2024-12-19 15:19:19 -0300

    Update ctrl_semaforo.h

--- main/utils/ctrl_semaforo.h
+++ main/utils/ctrl_semaforo.h
@@ -10,19 +10,26 @@ extern int tranca_salao;
 extern int count_avaliacao;
 extern int count_padawans_dentro; 
 extern int count_padawans_avaliados;
-extern int count_padawans_ajoelhado;
 extern int count_padawans_testados;
 extern int count_spec_dentro;
 
 
 extern sem_t capacidade_padawan; 
 extern sem_t capacidade_espectadores; 
+extern sem_t exclusao_mutua;
 extern sem_t avaliacao_padawan; 
 extern sem_t corte_tranca;
 extern sem_t capacidade_testes;
 extern sem_t cumprimentar_mestres;
-extern sem_t exclusao_mutua;
+
 extern sem_t padawan_ajoelhado;
+extern sem_t padawan_espera_avaliacao;
+extern sem_t padawan_finalizado;
+
+extern sem_t padawans_prontos;
+extern sem_t ajoelhados_sem;
+extern sem_t padawans_levantar;
+extern sem_t saida_padawans;
 
 // Inicialização e destruição dos semáforos
 void inicializa_semaforos(int max_espectadores, int max_padawans);
